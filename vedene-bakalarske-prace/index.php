2e0a6af8a51928c997d5092470403fb2209e3843
Ivan Dolnák <ivan.dolnak@connet-group.sk>
2018-08-13 12:58:49 +0200

    Creation of more descriptive list of bachelor thesis

--- vedene-bakalarske-prace/index.php
+++ vedene-bakalarske-prace/index.php
@@ -9,48 +9,232 @@ $p_lead = '';
 
 $content = <<<MAIN
 
-<div class="jumbotron jumbotron-fluid bg-light border-bottom text-center">
+<div class="jumbotron jumbotron-fluid bg-light bg-image border-bottom text-center">
   <div class="container">
     <h1 class="display-4">$h1</h1>
     <p class="lead">$p_lead</p>
   </div>
 </div>
 <div class="container">
-  <h2>Od roku 2009 po súčasnosť</h2>
-  <ul>
-    <li>Implementácia operačného systému RouterOS v prostredí IP sietí</li>
-    <li>Smerovanie v IP sieťach využitím operačného systému Unix/Linux</li>
-    <li>Implementácia protokolov redundancie v LAN</li>
-    <li>Bezpečná autentifikácia a autorizácia v LAN</li>
-    <li>Implementácia protokolu IPv6 v LAN</li>
-    <li>Poštový server Postfix s antispam podporou</li>
-    <li>Smerovanie medzi autonómnymi systémami v Internete prostredníctvom protokolu BGP</li>
-    <li>Virtuálna privátna sieť prostredníctvom softvéru OpenVPN</li>
-    <li>Prevádzka DHCP servera odolného voči výpadkom</li>
-    <li>Bezpečnosť bezdrôtovej komunikácie v 802.11 s použitím WEP</li>
-    <li>Šírenie multimediálneho obsahu v LAN vysielaním typu multicast</li>
-    <li>Možnosti použitia technológie MPLS v podnikových sieťach</li>
-    <li>Implementácia STP a RSTP protokolov v LAN sieťach</li>
-    <li>Možnosti použitia CDP a LLDP protokolov vo VoIP sieťach</li>
-    <li>Monitorovanie aktívnych sieťových prvkov prostredníctvom protokolov SNMP a RMON</li>
-    <li>Možnosti distribúcie informácií o VLAN v rozsiahlych LAN sieťach</li>
-    <li>Virtuálna privátna sieť prostredníctvom protokolov IPsec</li>
-    <li>Implementácia VoIP v LAN sieti prostredníctvom Cisco Communication Manager Express</li>
-    <li>Návrh sieťového firewallu na báze NanoBSD</li>
-    <li>Vplyv Cisco CEF na zasielanie IP paketov</li>
-    <li>Analýza Cloud služieb pre podnikové prostredie</li>
-    <li>Elektronický podpis v zmysle platnej legislatívy SR</li>
-    <li>Proprietárne Mikrotik technológie v IEEE 802.11 bezdrôtových sieťach</li>
-    <li>Návrh štruktúrovanej kabeláže pre LAN sieť v definovanom stavebnom objekte</li>
-    <li>Implementácia SSL certifikátov v prostredí webu</li>
-    <li>Minimalizácia doby konvergencie OSPF protokolu</li>
-    <li>Statické smerovanie v IPv6 sieťach</li>
-    <li>Implementácia RSTP  protokolu v LAN sieťach</li>
-    <li>Pasívne optické siete GPON</li>
-    <li>Dynamické smerovanie v IPv6 sieťach prostredníctvom protokolu RIPng</li>
-    <li>Virtuálna privátna sieť prostredníctvom protokolu L2TPv3</li>
-    <li>Vplyv OSPF oblastí na obsah smerovacej tabuľky</li>
-    </ul>
+  <h2>Rok odovzdania: 2009</h2>
+  <ol>
+    <li>
+      <strong>Bezpečná autentifikácia a&nbsp;autorizácia v&nbsp;LAN</strong>
+      <em>Autor:</em> Pavel Košlab
+    </li>
+    <li>
+      <strong>Implementácia operačného systému RouterOS v&nbsp;prostredí IP sietí</strong>
+      <em>Autor:</em> Peter Brezáni
+    </li>
+    <li>
+      <strong>Implementácia protokolov redundancie v&nbsp;LAN</strong>
+      <em>Autor:</em> Matej Kochan
+    </li>
+    <li>
+      <strong>Smerovanie v&nbsp;IP sieťach využitím operačného systému Unix/Linux</strong>
+      <em>Autor:</em> František Kavecký
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2010</h2>
+  <ol start="5">
+    <li>
+      <strong>Bezpečnosť bezdrôtovej komunikácie v&nbsp;802.11 s&nbsp;použitím WEP</strong>
+      <em>Autor:</em> Peter Šurina
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=656F2D580309C9D6A82B8A658777" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Implementácia protokolu IPv6 v&nbsp;LAN</strong>
+      <em>Autor:</em> Daniel Hudec
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=656F2D580309C9D6AB2383658777" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Poštový server Postfix s&nbsp;antispam podporou</strong>
+      <em>Autor:</em> Pavol Jurko
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=656F2D580309C9D6AB2380658777" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Prevádzka DHCP servera odolného voči výpadkom</strong>
+      <em>Autor:</em> Jozef Rajský
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=656F2D580309C9D6A82F81658777" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Smerovanie medzi autonómnymi systémami v&nbsp;Internete prostredníctvom protokolu BGP</strong>
+      <em>Autor:</em> Martin Orieščik
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=656F2D580309C9D6AB2284658777" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Šírenie multimediálneho obsahu v&nbsp;LAN vysielaním typu multicast</strong>
+      <em>Autor:</em> Vincent Vlk
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=656F2D580309C9D6AB2E85658777" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Virtuálna privátna sieť prostredníctvom softvéru OpenVPN</strong>
+      <em>Autor:</em> Martin Ráchela
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=656F2D580309C9D6AB228A658777" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2011</h2>
+  <ol start="12">
+    <li>
+      <strong>Implementácia STP a&nbsp;RSTP protokolov v&nbsp;LAN sieťach</strong>
+      <em>Autor:</em> Juraj Kramár
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=0218399C17E4BE6E1CA38AE813BF" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Monitorovanie aktívnych sieťových prvkov prostredníctvom protokolov SNMP a&nbsp;RMON</strong>
+      <em>Autor:</em> Jana Šajgalíková
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=0218399C17E4BE6E12A284E813BF" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Možnosti distribúcie informácií o&nbsp;VLAN v&nbsp;rozsiahlych sieťach</strong>
+      <em>Autor:</em> Michal Vršanský
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=0218399C17E4BE6E12AE84E813BF" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Možnosti použitia CDP a&nbsp;LLDP protokolov vo VoIP sieťach</strong>
+      <em>Autor:</em> Martin Žiak
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=0218399C17E4BE6E13A685E813BF" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2012</h2>
+  <ol start="16">
+    <li>
+      <strong>Virtuálna privátna sieť prostredníctvom protokolov IPsec</strong>
+      <em>Autor:</em> Veronika Chreneková
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=F5F9E12263A2DE487603727C4339" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2013</h2>
+  <ol start="17">
+    <li>
+      <strong>Možnosti použitia technológie MPLS v&nbsp;podnikových sieťach</strong>
+      <em>Autor:</em> Matej Paciga
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=EA5269F01D24DAD87AB8D57CA21B" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2014</h2>
+  <ol start="18">
+    <li>
+      <strong>Implementácia VoIP v&nbsp;LAN sieti prostredníctvom Cisco Communication Manager Express</strong>
+      <em>Autor:</em> Luboš Zuzčák
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=4A57AF3C5B51A495BD044B597F82" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2015</h2>
+  <ol start="19">
+    <li>
+      <strong>Analýza Cloud služieb pre podnikové prostredie</strong>
+      <em>Autor:</em> Peter Kopecký
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=63B76DDB09E75D01463AEE4D4D47" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Elektronický podpis v&nbsp;zmysle platnej legislatívy SR</strong>
+      <em>Autor:</em> Peter Vozár
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=38142DACDE934218CF42B4128AD1" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Návrh sieťového firewallu na báze NanoBSD</strong>
+      <em>Autor:</em> Ján Vozár
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=38142DACDE934218CF47B1128AD1" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Vplyv Cisco CEF na zasielanie IP paketov</strong>
+      <em>Autor:</em> Tomáš Benčík
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=38142DACDE934218CC44B6128AD1" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2016</h2>
+  <ol start="23">
+    <li>
+      <strong>Návrh štruktúrovanej kabeláže pre LAN sieť v&nbsp;definovanom stavebnom objekte</strong>
+      <em>Autor:</em> Štefan Lašo
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=94F27843EBD77D7A033842FA680C" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Proprietárne MikroTik technológie v&nbsp;IEEE 802.11 bezdrôtových sieťach</strong>
+      <em>Autor:</em> Michal Macura
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=093FABE64BBAAF12063FF36F11AD" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2017</h2>
+  <ol start="25">
+    <li>
+      <strong>Implementácia SSL certifikátov v&nbsp;prostredí webu</strong>
+      <em>Autor:</em> Michal Mihálik
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=691191F73B2EFEEBEBA5A8823DCA" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Minimalizácia doby konvergencie OSPF protokolu</strong>
+      <em>Autor:</em> Pavol Biž
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=25093AD9EB782982048C9E1EB8E1" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2018</h2>
+  <ol start="27">
+    <li>
+      <strong>Dynamické smerovanie v&nbsp;IPv6 sieťach prostredníctvom protokolu RIPng</strong>
+      <em>Autor:</em> Miroslav Valovič
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=65CF89236B0A7E124DD77AE040F7" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Implementácia RSTP protokolu v&nbsp;LAN sieťach</strong>
+      <em>Autor:</em> Peter Kamenský
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=65CF89236B0A7E1242D079E040F7" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Pasívne optické siete GPON</strong>
+      <em>Autor:</em> Ján Šuška
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=65CF89236B0A7E1242D078E040F7" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Statické smerovanie v&nbsp;IPv6 sieťach</strong>
+      <em>Autor:</em> Jaroslav Beredi
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=65CF89236B0A7E1241D072E040F7" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2019</h2>
+  <ol start="31">
+    <li>
+      <strong>Virtuálna privátna sieť prostredníctvom protokolu L2TPv3</strong>
+      <em>Autor:</em> Dávid Matula
+    </li>
+    <li>
+      <strong>Vplyv OSPF oblastí na obsah smerovacej tabuľky</strong>
+      <em>Autor:</em> Ján Václavik
+    </li>
+  </ol>
+</div>
+<div class="container">
+  <p>
+    <strong>Mohli by vás zaujímať aj</strong>
+    <a href="https://www.dolnak.eu/vedene-diplomove-prace/">vedené diplomové práce</a>
+  </p>
 </div>
 
 MAIN;
