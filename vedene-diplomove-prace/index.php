8f19f49cbe9318741d6af49d0de0d128fbe57cb7
Ivan Dolnák <ivan.dolnak@connet-group.sk>
2018-08-15 00:01:30 +0200

    Content update

--- vedene-diplomove-prace/index.php
+++ vedene-diplomove-prace/index.php
@@ -15,51 +15,278 @@ $content = <<<MAIN
     <p class="lead">$p_lead</p>
   </div>
 </div>
-<div class="container text-justify">
-  <h2>Od roku 2009 po súčasnosť</h2>
+<div class="container">
+  <h2>Rok odovzdania: 2009</h2>
   <ol>
-    <li>Bezpečnosť VoIP</li>
-    <li>Metódy synchronizácie presného systémového času v LAN</li>
-    <li>Generátor paketov protokolu SIP v jazyku Perl</li>
-    <li>Analýza doby konvergencie dynamických smerovacích protokolov</li>
-    <li>Zber a uchovávanie elektronickej komunikácie v zmysle platnej legislatívy SR</li>
-    <li>Smerovanie medzi OSPF oblasťami prostredníctvom softvérového balíka Quagga</li>
-    <li>Zabezpečenie redundancie predvolenej brány v LAN prostredníctvom VRRP protokolu</li>
-    <li>Autentifikácia, autorizácia a účtovanie prístupu používateľov do LAN prostredníctvom protokolu IEEE 802.1x</li>
-    <li>Implementácia QoS v LAN s podporou VoIP služieb</li>
-    <li>Smerovanie protokolu IPv6 so zameraním na interoperabilitu s IPv4 sieťami</li>
-    <li>Smerovanie protokolu IPv6 so zameraním na WAN siete</li>
-    <li>Poštový server Postfix s podporou trvalého ukladania elektronickej pošty</li>
-    <li>Zabezpečenie hraničného smerovača proti sieťovým útokom</li>
-    <li>Škálovateľnosť protokolu iBGP v rámci autonómneho systému</li>
-    <li>Riadenie prístupu vo virtuálnych privátnych sieťach</li>
-    <li>Aktualizácia DNS záznamov prostredníctvom DHCP servera</li>
-    <li>Bezpečnosť bezdrôtovej komunikácie v 802.11 s použitím WPA</li>
-    <li>Šírenie multimediálneho obsahu vysielaním typu multicast</li>
-    <li>Virtuálna privátna sieť prostredníctvom MPLS VPN</li>
-    <li>Systémy detekcie a prevencie sieťových útokov IDS/IPS</li>
-    <li>Smerovanie multimediálneho toku šíreného pomocou skupinového vysielania</li>
-    <li>Špecifiká dynamických smerovacích protokolov vo Frame Relay sieťach</li>
-    <li>Systém DNS s podporou IPv6 protokolu</li>
-    <li>Redistribúcia medzi smerovacími doménami</li>
-    <li>Technológie VPN sietí typu IPsec v podnikovom prostredí</li>
-    <li>Minimalizácia doby konvergencie dynamických smerovacích protokolov</li>
-    <li>Systém DNS s podporou DNSSEC rozšírenia</li>
-    <li>Implementácia QoS v Mikrotik RouterOS</li>
-    <li>Lokálna klasifikácia a značkovanie pre účely QoS v MikroTik RouterOS</li>
-    <li>Virtuálna privátna sieť prostredníctvom MPLS AToM</li>
-    <li>Implementácia BGP Route Servera v internetových peeringových centrách</li>
-    <li>Detekcia a prevencia sieťových útokov IDS/IPS systémami</li>
-    <li>Implementácia DMVPN sietí v podnikovom prostredí</li>
-    <li>Technológie VPN sietí typu PPTP a L2TP v podnikovom prostredí</li>
-    <li>Traffic Engineering v Mikrotik RouterOS</li>
-    <li>Interoperabilita implementácií protokolu OpenVPN v operačných systémoch MikroTik RouterOS a Linux</li>
-    <li>Implementácia SSL certifikátu doplnená o bezpečnostné hlavičky protokolov HTTP a HTTPS</li>
-    <li>Redistribúcia medzi smerovacími doménami protokolov EIGRP a OSPF</li>
-    <li>Technológie VPN sietí typu MPLS AToM v podnikovom prostredí</li>
+    <li>
+      <strong>Bezpečnosť VoIP</strong>
+      <em>Autor:</em> Bc. Tomáš Hrachala
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2010</h2>
+  <ol start="2">
+    <li>
+      <strong>Analýza doby konvergencie dynamických smerovacích protokolov</strong>
+      <em>Autor:</em> Bc. Andrea Šugárová
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=B7849FF1DE1334BAB7AB22A59918" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Generátor paketov protokolu SIP v&nbsp;jazyku Perl</strong>
+      <em>Autor:</em> Bc. Tomáš Iskra
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=2E010DF3E4CBC231732B2EF443FC" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Metódy synchronizácie presného systémového času v&nbsp;LAN</strong>
+      <em>Autor:</em> Bc. Erika Fullajtárová
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=B7849FF1DE1334BAB7A922A59918" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2011</h2>
+  <ol start="5">
+    <li>
+      <strong>Autentifikácia, autorizácia a&nbsp;účtovanie prístupu používateľov do LAN prostredníctvom protokolu IEEE 802.1x</strong>
+      <em>Autor:</em> Bc. Pavel Košlab
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=9CE455D2677453A5303ACC027702" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Implementácia QoS v&nbsp;LAN s&nbsp;podporou VoIP služieb</strong>
+      <em>Autor:</em> Bc. Ján Lamoš
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=0218399C17E4BE6E11A187E813BF" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Smerovanie medzi OSPF oblasťami prostredníctvom softvérového balíka Quagga</strong>
+      <em>Autor:</em> Bc. František Kavecký
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=0218399C17E4BE6E11A080E813BF" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Správa vyhradených zariadení cez verejne prístupné siete</strong>
+      <em>Autor:</em> Bc. Martin Jakubčák
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=71F94544F0776B3D28C82BCA50CE" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Zber a&nbsp;uchovávanie elektronickej komunikácie v&nbsp;zmysle platnej legislatívy SR</strong>
+      <em>Autor:</em> Bc. Peter Brezáni
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=5307003F770ACE0270822534961F" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2012</h2>
+  <ol start="10">
+    <li>
+      <strong>Aktualizácia DNS záznamov prostredníctvom DHCP servera</strong>
+      <em>Autor:</em> Bc. Jozef Rajský
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=8DFABC8C09FABED70FB3FA750407" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Bezpečnosť bezdrôtovej komunikácie v&nbsp;802.11 s&nbsp;použitím WPA</strong>
+      <em>Autor:</em> Bc. Peter Šurina
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=8DFABC8C09FABED70FBDFE750407" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Poštový server Postfix s&nbsp;podporou trvalého ukladania elektronickej pošty</strong>
+      <em>Autor:</em> Bc. Pavol Jurko
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=C87B0DC5E3BB964116E6DC818551" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Riadenie prístupu vo virtuálnych privátnych sieťach</strong>
+      <em>Autor:</em> Bc. Martin Ráchela
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=C87B0DC5E3BB964116E6DD818551" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Smerovanie protokolu IPv6 so zameraním na interoperabilitu s&nbsp;IPv4 sieťami</strong>
+      <em>Autor:</em> Bc. Matúš Benovič
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=8DFABC8C09FABED70FB0FD750407" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Smerovanie protokolu IPv6 so zameraním na WAN siete</strong>
+      <em>Autor:</em> Bc. Daniel Hudec
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=8DFABC8C09FABED70FB2FE750407" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Šírenie multimediálneho obsahu vysielaním typu multicast</strong>
+      <em>Autor:</em> Bc. Vincent Vlk
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=290ED8FCEA4BB1346AB774CFCCA7" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Škálovateľnosť protokolu iBGP v&nbsp;rámci autonómneho systému</strong>
+      <em>Autor:</em> Bc. Martin Orieščik
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=2EA293C8B5A34C4569319F942649" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Zabezpečenie hraničného smerovača proti sieťovým útokom</strong>
+      <em>Autor:</em> Bc. Martin Michalko
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=C87B0DC5E3BB964117E9D0818551" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Zabezpečenie redundancie predvolenej brány v&nbsp;LAN prostredníctvom VRRP protokolu</strong>
+      <em>Autor:</em> Bc. Matej Kochan
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=48914484AAB39F5B960153F8116B" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2013</h2>
+  <ol start="20">
+    <li>
+      <strong>Smerovanie multimediálneho toku šíreného pomocou skupinového vysielania</strong>
+      <em>Autor:</em> Bc. Juraj Kramár
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=9359D1AC2B33EC8F6885FEDC0F9A" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Systémy detekcie a&nbsp;prevencie sieťových útokov IDS/IPS</strong>
+      <em>Autor:</em> Bc. Martin Berzéty
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=EE95B465952BB5C0020145B19651" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Virtuálna privátna sieť prostredníctvom MPLS VPN</strong>
+      <em>Autor:</em> Bc. Martin Behro
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=DBEBBE2C2F1C2B14A582E1D96E82" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2014</h2>
+  <ol start="23">
+    <li>
+      <strong>Minimalizácia doby konvergencie dynamických smerovacích protokolov</strong>
+      <em>Autor:</em> Bc. Peter Kontura
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=591608704B394480C61FDBCFFE0D" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Redistribúcia medzi smerovacími doménami</strong>
+      <em>Autor:</em> Bc. Tomáš Dugas
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=591608704B394480C61DD1CFFE0D" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Systém DNS s&nbsp;podporou IPv6 protokolu</strong>
+      <em>Autor:</em> Bc. Juraj Ducký
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=062C1ED5E40068C516D510B54EF2" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Špecifiká dynamických smerovacích protokolov vo Frame Relay sieťach</strong>
+      <em>Autor:</em> Bc. Martin Žiak
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=2B30D2A570A51E648D9DAB9DF416" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Technológie VPN sietí typu IPsec v&nbsp;podnikovom prostredí</strong>
+      <em>Autor:</em> Bc. Veronika Chreneková
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=512CD14455EC7E23F85D03245F73" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2015</h2>
+  <ol start="28">
+    <li>
+      <strong>Systém DNS s&nbsp;podporou DNSSEC rozšírení</strong>
+      <em>Autor:</em> Bc. Martin Repčík
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=203CA706F79D2AD723A2ABC65647" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2016</h2>
+  <ol start="29">
+    <li>
+      <strong>Implementácia QoS v&nbsp;MikroTik RouterOS</strong>
+      <em>Autor:</em> Bc. Michal Mikula
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=CAF43C23BCEFEBA4BFCF1BBCB6F5" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Lokálna klasifikácia a&nbsp;značkovanie pre účely QoS v&nbsp;MikroTik RouterOS</strong>
+      <em>Autor:</em> Bc. Lukáš Bôtoš
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=E725DF4FE130A8068B6E57018B4B" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Virtuálna privátna sieť prostredníctvom MPLS AToM</strong>
+      <em>Autor:</em> Bc. Luboš Zuzčák
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=7A24B9B1E4B8C85D548C487807C5" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2017</h2>
+  <ol start="32">
+    <li>
+      <strong>Detekcia a&nbsp;prevencia sieťových útokov IDS/IPS systémami</strong>
+      <em>Autor:</em> Bc. Tomáš Benčík
+      <em>CRZP:</em>
+      <a href="" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Implementácia BGP Route Servera v&nbsp;internetových peeringových centrách</strong>
+      <em>Autor:</em> Bc. Martin Všelko
+      <em>CRZP:</em>
+      <a href="" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Implementácia DMVPN sietí v&nbsp;podnikovom prostredí</strong>
+      <em>Autor:</em> Bc. Ivan Lonc
+      <em>CRZP:</em>
+      <a href="" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Technológie VPN sietí typu PPTP a&nbsp;L2TP v&nbsp;podnikovom prostredí</strong>
+      <em>Autor:</em> Bc. Lukáš Greisiger
+      <em>CRZP:</em>
+      <a href="" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Traffic Engineering v&nbsp;MikroTik RouterOS</strong>
+      <em>Autor:</em> Bc. Vladimír Porubiak
+      <em>CRZP:</em>
+      <a href="" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2018</h2>
+  <ol start="37">
+    <li>
+      <strong>Implementácia SSL certifikátu doplnená o&nbsp;bezpečnostné hlavičky protokolov HTTP a&nbsp;HTTPS</strong>
+      <em>Autor:</em> Bc. Lukáš Martaus
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=DD9282570EFDEA065773ABAC0787" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Interoperabilita implementácií protokolu OpenVPN v&nbsp;operačných systémoch MikroTik RouterOS a&nbsp;Linux</strong>
+      <em>Autor:</em> Bc. Miroslav Dančík
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=DD9282570EFDEA065679AEAC0787" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+    <li>
+      <strong>Technológie VPN sietí typu MPLS AToM v&nbsp;podnikovom prostredí</strong>
+      <em>Autor:</em> Bc. Martin Šušolík
+      <em>CRZP:</em>
+      <a href="https://opac.crzp.sk/?fn=detailBiblioForm&sid=65CF89236B0A7E1241D073E040F7" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+    </li>
+  </ol>
+  <h2>Rok odovzdania: 2019</h2>
+  <ol start="40">
+    <li>
+      <strong>Redistribúcia medzi smerovacími doménami protokolov EIGRP a&nbsp;OSPF</strong>
+      <em>Autor:</em> Bc. Stanislav Porubčanský
+    </li>
   </ol>
 </div>
-<div class="container text-justify">
+<div class="container">
   <p>
     <strong>Mohli by vás zaujímať aj</strong>
     <a href="https://www.dolnak.eu/vedene-bakalarske-prace/">vedené bakalárske práce</a>
